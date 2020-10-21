fcd4f63e96e44e2a5edab2c336866d5e0ead9b50
Amnay shobak <ashobak.c@stc.com.sa>
2020-10-21 21:29:30 +0200

    change color , fixed mobile add style for arabic

---
+++ js/main-ar.js
@@ -0,0 +1,90 @@
+// home slider
+$('.home-slider').owlCarousel({
+    rtl: true,
+    loop: true,
+    autoplay: true,
+    margin: 10,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: true,
+    autoplayHoverPause: true,
+    items: 1,
+    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 1,
+            nav: false
+        },
+        1000: {
+            items: 1,
+            nav: true
+        }
+    }
+});
+
+// owl carousel
+var majorCarousel = $('.js-carousel-1');
+majorCarousel.owlCarousel({
+    rtl: true,
+    loop: true,
+    autoplay: true,
+    stagePadding: 7,
+    margin: 20,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: true,
+    autoplayHoverPause: true,
+    items: 3,
+    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: false
+        }
+    }
+});
+
+// owl carousel
+var major2Carousel = $('.js-carousel-2');
+major2Carousel.owlCarousel({
+    rtl: true,
+    loop: true,
+    autoplay: true,
+    stagePadding: 7,
+    margin: 20,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: true,
+    autoplayHoverPause: true,
+    items: 4,
+    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 3,
+            nav: false
+        },
+        1000: {
+            items: 4,
+            nav: true,
+            loop: false
+        }
+    }
+});
+
